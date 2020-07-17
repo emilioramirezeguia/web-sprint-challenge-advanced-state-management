56f68f74464cd62621a611860fd19f61513056a2
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-07-17 08:27:03 -0700

    Create Redux store with 'empty' reducer

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { reducer } from "./store/reducers";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
 import App from "./components/App";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(reducer, applyMiddleware(thunk, logger));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
